41f8d237482eaf505bbd134dcc42e028d946e79c
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2008-08-30 20:46:40 +0000

    checked in for 2_1_4

---
+++ test/castor_cfg.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("CastorProducts")
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(20)
+)
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:digiout_test.root')
+)
+
+process.fromRecHits = cms.EDFilter("Castor",
+    FullReco = cms.untracked.bool(True),
+    Egamma_maxDepth = cms.untracked.double(14488.0),
+    Egamma_maxWidth = cms.untracked.double(0.2),
+    KtRecombination = cms.untracked.uint32(2),
+    Egamma_minRatio = cms.untracked.double(0.5),
+    KtrParameter = cms.untracked.double(1.0)
+)
+
+process.MyOutputModule = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring('keep *_fromRecHits_*_*'),
+    fileName = cms.untracked.string('test_output.root')
+)
+
+process.producer = cms.Path(process.fromRecHits)
+process.end = cms.EndPath(process.MyOutputModule)
+
